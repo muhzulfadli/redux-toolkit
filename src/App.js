a842f218500908b12cca754c7635ece1bcf5b214
Zulfadli <muh.fadli987@gmail.com>
2022-04-25 08:52:02 +0800

    final commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import counterSlice from './store/counter'
+import Burger from './Component/Burger/Burger'
+import Product from './Component/Product/Product'
+
+function App() {
+
+  const count = useSelector( store => store.counter.count )
+  const dispatch = useDispatch()
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={ () => dispatch( counterSlice.actions.increment() ) }>INCREMENT</button>
+      <button onClick={ () => dispatch( counterSlice.actions.decrement() ) }>DECREMENT</button>
+      <button onClick={ () => dispatch( counterSlice.actions.increaseByNumber(5) ) }>Increment by 5</button>
+      <button onClick={ () => dispatch( counterSlice.actions.increaseByNumber(10) ) }>Increment by 10</button>
+      <hr />
+      <Burger />
+      <hr />
+      <Product />
+    </div>
+  )
+
+}
+
+export default App
